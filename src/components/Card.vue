a21882b900e7b709e0339d45935bbf2ad7e3c9af
Yangkai He <yh952@cornell.edu>
2021-11-14 20:32:31 -0500

    Added Card View and Card Coponent.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="card">
-    <h1>Display card</h1>
+  <div class="cardBox">
+    <b-card :img-src="imgsrc" img-alt="Card image" img-left class="mb-3">
+      <b-card-text>
+        <div class="card-content">
+          <h1>{{ item.name }}</h1>
+          <p>Time: {{ item.time }} min</p>
+          <p>Serving: {{ item.serving }}</p>
+        </div>
+      </b-card-text>
+    </b-card>
   </div>
 </template>
 
@@ -9,13 +17,53 @@ export default {
   name: "Card",
   data: function () {
     return {
+      imgsrc: "",
+      item: [],
     };
   },
-  methods: {
-    
+  props: {
+    itemInfo: {
+      type: Object,
+    },
   },
+  created() {
+    //this.$set(this.itemInfo); //父组件传值时无法触发子组件watch监听, 利用set方法触发页面重新渲染
+  },
+  watch: {
+    test: {
+      immediate: true,
+      handler: function () {
+        this.item = this.itemInfo;
+        this.imgsrc = require("/data/" + this.item.cover);
+        //this.imgsrc = "https://placekitten.com/300/300";
+        console.log("Item info got:", this.item);
+      },
+      deep: true,
+    },
+  },
+  methods: {},
 };
 </script>
 
 <style>
+.cardBox {
+  margin: 100px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+.card-body {
+  text-align: left;
+}
+
+.card-content {
+  padding: 50px;
+}
+
+.card-img-left {
+  height: 20%;
+  width: 30%;
+  object-fit: cover;
+  object-position: center;
+}
 </style>
--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -4,8 +4,9 @@
     <div class="searchBar">
       <SearchBar v-show="showSearch" @send="outputData" />
     </div>
-    <div class="cards">
-      <Card />
+
+    <div class="cards" v-for="(item,index) in recipes" :key="index">
+      <Card :itemInfo="item" />
     </div>
   </div>
 </template>
@@ -13,33 +14,38 @@
 <script>
 import SearchBar from "@/components/SearchBar.vue";
 import Card from "@/components/Card.vue";
+import RecipeData from "../../data/mock_data.json";
 
 export default {
   name: "CardView",
   components: {
     SearchBar,
-    Card
+    Card,
   },
   data() {
     return {
-      inputData: [],
       showSearch: true,
-      time: 30,
-      serving: 5,
-      ingredients: [],
+      inputData: [],
+      recipes: [],
     };
+  },
+  created(){
+    this.getRecipes();
   },
   methods: {
     outputData: function (data) {
       this.inputData = data;
       console.log("Card Received:", data);
-      //this.showInputTask = false;
+    },
+    getRecipes: function () {
+      this.recipes = RecipeData
+      console.log('Card View: ', this.recipes)
     },
   },
 };
 </script>
 <style>
 body {
-    text-align: center;
+  text-align: center;
 }
 </style>
