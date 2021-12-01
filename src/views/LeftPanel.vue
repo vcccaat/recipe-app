00a951459dd4f4c732b31456ea557bcf1744ee38
Yangkai He <yh952@cornell.edu>
2021-11-30 23:45:09 -0500

    Yangkai: First version of dynamic left panel

--- src/views/LeftPanel.vue
+++ src/views/LeftPanel.vue
@@ -1,38 +1,44 @@
 <template>
-	<div id="left-panel">
-		<div style="text-align: right">
-			<i class="bi bi-arrow-bar-left" style="font-size: 25px"></i>
-		</div>
-		<h5>How long would you like to spend on this meal?</h5>
-		<div class="d-flex">
-			<span class="">0</span>
-			<form class="flex-grow-1">
-				<input @change="handleTimeChange" v-model="time" style="width: 100%" type="range" min="0" max="100" />
-			</form>
-			<span class="">100 Min</span>
-		</div>
-		<div>{{ time }} Min</div>
+	<div >
+		<div id="left-panel" class="fullPanel" v-if="showPanel">
+			<div style="text-align: right">
+				<button @click="onHide" class="bi bi-arrow-bar-left" style="font-size: 25px"></button>
+			</div>
+			<h5>How long would you like to spend on this meal?</h5>
+			<div class="d-flex">
+				<span class="">0</span>
+				<form class="flex-grow-1">
+					<input @change="handleTimeChange" v-model="time" style="width: 100%" type="range" min="0" max="100" />
+				</form>
+				<span class="">100 Min</span>
+			</div>
+			<div>{{ time }} Min</div>
 
-		<h5>How many people do you need to serve?</h5>
-		<div class="d-flex">
-			<span class="">0</span>
-			<form class="flex-grow-1">
-				<input @change="handleServingChange" v-model="serving" style="width: 100%" class="border-0" type="range" min="0" max="10" />
-			</form>
-			<span class="">10</span>
-		</div>
-		<div>{{ serving }} People</div>
+			<h5>How many people do you need to serve?</h5>
+			<div class="d-flex">
+				<span class="">0</span>
+				<form class="flex-grow-1">
+					<input @change="handleServingChange" v-model="serving" style="width: 100%" class="border-0" type="range" min="0" max="10" />
+				</form>
+				<span class="">10</span>
+			</div>
+			<div>{{ serving }} People</div>
 
-		<div class="Diary" v-for="item in ingredients" :key="item.type">
-			<h5>{{ item.type }}</h5>
-			<table class="buttons" v-for="i in Math.ceil(item.ingredients.length / 4)" :key="i">
-				<tr>
-					<td v-for="ind in item.ingredients.slice((i - 1) * 4, i * 4)" :key="ind"><Button :name="ind" :id="ind" /></td>
-				</tr>
-			</table>
+			<div class="Diary" v-for="item in ingredients" :key="item.type">
+				<h5>{{ item.type }}</h5>
+				<table class="buttons" v-for="i in Math.ceil(item.ingredients.length / 4)" :key="i">
+					<tr>
+						<td v-for="ind in item.ingredients.slice((i - 1) * 4, i * 4)" :key="ind"><Button :name="ind" :id="ind" /></td>
+					</tr>
+				</table>
+			</div>
+			<button type="button" class="btn btn-light" @click="handleClear">Clear</button>
+			<button type="button" class="btn btn-primary" @click="handleConfirm">Confirm</button>
 		</div>
-		<button type="button" class="btn btn-light" @click="handleClear">Clear</button>
-		<button type="button" class="btn btn-primary" @click="handleConfirm">Confirm</button>
+		
+		<div class="hidePanel" v-if="!showPanel">
+			<button @click="onHide" class="bi bi-arrow-bar-right" style="font-size: 25px"></button>
+		</div>
 	</div>
 </template>
 
@@ -45,10 +51,24 @@ import Ingredient from '../components/Ingredient.js';
 
 export default {
 	name: 'Home',
+	data() {
+		return {
+			time: 30,
+			serving: 5,
+			ingredients: [],
+			showPanel: true
+
+		};
+	},
 	created() {
 		this.getIngredients();
+		
 	},
 	methods: {
+		onHide() {
+			this.showPanel = !this.showPanel;
+			this.$emit('movePanel', this.showPanel)
+		},
 		getIngredients() {
 			this.ingredients = ingredientData;
 		},
@@ -64,21 +84,17 @@ export default {
 		handleClear() {
 			Ingredient.$emit('clear');
 		},
+		
 	},
 	components: {
 		Button,
 	},
-	data() {
-		return {
-			time: 30,
-			serving: 5,
-			ingredients: [],
-		};
-	},
+	
 };
 // slider reference: https://mdbootstrap.com/snippets/jquery/mdbootstrap/944578
 </script>
 <style>
+
 .buttons {
 	width: 100%;
 	table-layout: fixed;
