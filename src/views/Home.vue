123898c30aac2745a32dc7acf8351abfa43ff50e
Yingshi Zhu <yz2698@cornell.edu>
2021-12-01 00:19:53 -0500

    Yingshi: fix bugs, change layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,6 +31,19 @@
   </div> -->
 	<div class="home">
 		<div class="row">
+			<div class="header-bar">
+				<div></div>
+				<div>
+					<el-popover placement="top-start" width="200" trigger="click" :content="'Your current reward points: ' + this.rewardPoints">
+						<el-button slot="reference" style="margin-right: 20px">Reward points</el-button>
+					</el-popover>
+					<router-link to="/reward">
+						<i class="bi bi-person-circle" style="font-size: 30px"></i>
+					</router-link>
+				</div>
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-sm-4">
 				<LeftPanel />
 			</div>
@@ -38,20 +51,12 @@
 				<b-modal ref="rewardPopup" id="modal-1" title="Welcome!" hide-footer>
 					<p>Login First Time Today! Reward Points +1!</p>
 				</b-modal>
-				<div class="header-bar">
-					<el-popover placement="top-start" width="200" trigger="click" :content="'Your current reward points: ' + this.rewardPoints">
-						<el-button slot="reference">Reward points</el-button>
-					</el-popover>
-					<router-link to="/reward">
-						<i class="bi bi-person-circle" style="font-size: 30px"></i>
-					</router-link>
-					<div>
-						<SearchBar v-show="showSearch" @search-sent="search" :errMsg="errMsg" :showError="showError" />
-						<div class="">
-							<div class="card-container">
-								<div class="cards" v-for="(item, index) in filteredList" :key="index">
-									<Card :recipe="item" />
-								</div>
+				<div>
+					<SearchBar v-show="showSearch" @search-sent="search" :errMsg="errMsg" :showError="showError" />
+					<div class="">
+						<div class="card-container">
+							<div class="cards" v-for="(item, index) in filteredList" :key="index">
+								<Card :recipe="item" />
 							</div>
 						</div>
 					</div>
@@ -219,12 +224,17 @@ export default {
 }
 
 .header-bar {
-	text-align: right;
-	margin: 20px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 2vh;
+	/* padding-bottom: 10px; */
+	border-bottom: 2px solid #ddd;
+	box-shadow: 6px 8px 6px -6px rgb(0 0 0 / 20%);
 }
-.header-bar > * {
+/* .header-bar > * {
 	margin: 20px;
-}
+} */
 
 .card-container {
 	display: flex;
--- src/views/RewardView.vue
+++ src/views/RewardView.vue
@@ -1,160 +1,163 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h2 class="title" style="font-weight: 800">Rewards</h2>
-      </div>
-    </div>
-
-    <div class="card cardReward">
-      <div class="row">
-        <div class="col-3"></div>
-        <div class="col-3" style="text-align: left">
-          <div class="row">
-            <span>Reward Points</span>
-          </div>
-          <div class="row">
-            <h1>{{ rewardPoints }}</h1>
-          </div>
-        </div>
-        <div class="col-3">
-          <img class="svg" :src="bowlIcon" />
-        </div>
-        <div class="col-3"></div>
-      </div>
-    </div>
-    <div class="row"><h5 style="text-align: left">Rewards</h5></div>
-
-    <div class="card cardReward">
-      <div class="row">
-        <div class="col-4">
-          <img class="svg" :src="vegIcon" />
-        </div>
-        <div class="col align-self-center" style="text-align: left">
-          <div class="row"><h5>20% Grocery Discount</h5></div>
-          <div class="row"><h5>on points: 2000</h5></div>
-          <button type="button" class="btn btn-outline-dark redeemBtn">
-            REDEEM
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="card cardReward">
-      <div class="row">
-        <div class="col-4">
-          <img class="svg" :src="vegIcon" />
-        </div>
-        <div class="col align-self-center" style="text-align: left">
-          <div class="row"><h5>15% Grocery Discount</h5></div>
-          <div class="row"><h5>on points: 1500</h5></div>
-          <button type="button" class="btn btn-outline-dark redeemBtn">
-            REDEEM
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="card cardReward">
-      <div class="row">
-        <div class="col-4">
-          <img class="svg" :src="vegIcon" />
-        </div>
-        <div class="col align-self-center" style="text-align: left">
-          <div class="row"><h5>10% Grocery Discount</h5></div>
-          <div class="row"><h5>on points: 1000</h5></div>
-          <button type="button" class="btn btn-outline-dark redeemBtn">
-            REDEEM
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="card cardReward">
-      <div class="row">
-        <div class="col-4">
-          <img class="svg" :src="vegIcon" />
-        </div>
-        <div class="col align-self-center" style="text-align: left">
-          <div class="row"><h5>5% Grocery Discount</h5></div>
-          <div class="row"><h5>on points: 500</h5></div>
-          <button type="button" class="btn btn-outline-dark redeemBtn">
-            REDEEM
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="row"><h5 style="text-align: left">My comments</h5></div>
-    <div class="card cardReward"  v-for="(item, index) in displayList" :key="index">
+	<div>
+		<div class="header-bar">
+			<router-link to="/">
+				<i class="bi bi-chevron-left"></i>
+			</router-link>
+			<div>
+				<el-popover placement="top-start" width="200" trigger="click" :content="'Your current reward points: ' + this.rewardPoints">
+					<el-button slot="reference" style="margin-right: 20px">
+						Reward Points
+						<!-- <i class="bi bi-cash-coin" style="font-size: 30px"></i> -->
+					</el-button>
+				</el-popover>
+				<router-link to="/reward">
+					<i class="bi bi-person-circle" style="font-size: 30px"></i>
+				</router-link>
+			</div>
+		</div>
+		<div class="container">
 			<div class="row">
-        <div class="col-6" style="text-align: left; padding-left: 50px">
-          <div class="row">
-            <h3 style="margin-bottom: 2px">{{item[0]}}</h3>
-          </div>
-          <div class="row">
-            <el-rate
-              disabled
-              v-model="item[2]"
-              :colors="colors">
-            </el-rate>
-          </div>
-          <div class="row">
-            <span style="margin: 2px 3px 2px"> {{item[1]}} </span>
-          </div>
-        </div>
-				<div class="col-6 block" style="text-align: right; padding-right: 50px">
-					<el-image
-					style="width: 100px; height: 100px; margin: 5px"
-					v-for="(img,i) in item.slice(3)" :key="i" :src="img"
-					fit="cover"></el-image>
+				<div class="col">
+					<h2 class="title" style="font-weight: 800">Rewards</h2>
 				</div>
 			</div>
-    </div>
-  </div>
+
+			<div class="card cardReward">
+				<div class="row">
+					<div class="col-3"></div>
+					<div class="col-3" style="text-align: left">
+						<div class="row">
+							<span>Reward Points</span>
+						</div>
+						<div class="row">
+							<h1>{{ rewardPoints }}</h1>
+						</div>
+					</div>
+					<div class="col-3">
+						<img class="svg" :src="bowlIcon" />
+					</div>
+					<div class="col-3"></div>
+				</div>
+			</div>
+			<div class="row"><h5 style="text-align: left">Rewards</h5></div>
+
+			<div class="card cardReward">
+				<div class="row">
+					<div class="col-4">
+						<img class="svg" :src="vegIcon" />
+					</div>
+					<div class="col align-self-center" style="text-align: left">
+						<div class="row"><h5>20% Grocery Discount</h5></div>
+						<div class="row"><h5>on points: 2000</h5></div>
+						<button type="button" class="btn btn-outline-dark redeemBtn">REDEEM</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="card cardReward">
+				<div class="row">
+					<div class="col-4">
+						<img class="svg" :src="vegIcon" />
+					</div>
+					<div class="col align-self-center" style="text-align: left">
+						<div class="row"><h5>15% Grocery Discount</h5></div>
+						<div class="row"><h5>on points: 1500</h5></div>
+						<button type="button" class="btn btn-outline-dark redeemBtn">REDEEM</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="card cardReward">
+				<div class="row">
+					<div class="col-4">
+						<img class="svg" :src="vegIcon" />
+					</div>
+					<div class="col align-self-center" style="text-align: left">
+						<div class="row"><h5>10% Grocery Discount</h5></div>
+						<div class="row"><h5>on points: 1000</h5></div>
+						<button type="button" class="btn btn-outline-dark redeemBtn">REDEEM</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="card cardReward">
+				<div class="row">
+					<div class="col-4">
+						<img class="svg" :src="vegIcon" />
+					</div>
+					<div class="col align-self-center" style="text-align: left">
+						<div class="row"><h5>5% Grocery Discount</h5></div>
+						<div class="row"><h5>on points: 500</h5></div>
+						<button type="button" class="btn btn-outline-dark redeemBtn">REDEEM</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="row"><h5 style="text-align: left">My comments</h5></div>
+			<div class="card cardReward" v-for="(item, index) in displayList" :key="index">
+				<div class="row">
+					<div class="col-6" style="text-align: left; padding-left: 50px">
+						<div class="row">
+							<h3 style="margin-bottom: 2px">{{ item[0] }}</h3>
+						</div>
+						<div class="row">
+							<el-rate disabled v-model="item[2]" :colors="colors"> </el-rate>
+						</div>
+						<div class="row">
+							<span style="margin: 2px 3px 2px"> {{ item[1] }} </span>
+						</div>
+					</div>
+					<div class="col-6 block" style="text-align: right; padding-right: 50px">
+						<el-image style="width: 100px; height: 100px; margin: 5px" v-for="(img, i) in item.slice(3)" :key="i" :src="img" fit="cover"></el-image>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 // import Button from '../components/Button.vue';
 export default {
-  created() {
-    this.rewardPoints = localStorage.getItem("rewardPoints", this.rewardPoints);
-    this.displayList = JSON.parse(localStorage.getItem('feedbackPics'))
-  },
+	created() {
+		this.rewardPoints = localStorage.getItem('rewardPoints', this.rewardPoints);
+		this.displayList = JSON.parse(localStorage.getItem('feedbackPics'));
+	},
 	mounted() {
 		window.addEventListener('storage', (e) => {
-			if (e.key && e.key=="feedbackPics" && e.newValue){
-				console.log("storage值发生变化后触发:", e)
-				this.displayList = JSON.parse(e.newValue)
+			if (e.key && e.key == 'feedbackPics' && e.newValue) {
+				console.log('storage值发生变化后触发:', e);
+				this.displayList = JSON.parse(e.newValue);
 				console.log(this.displayList);
 			}
-    });
+		});
 	},
-  methods: {},
-  components: {},
-  data() {
-    return {
-      bowlIcon: require("/public/img/icons/bowl.svg"),
-      vegIcon: require("/public/img/icons/vegetable.svg"),
-      rewardPoints: 10,
-			displayList: []
-    };
-  },
+	methods: {},
+	components: {},
+	data() {
+		return {
+			bowlIcon: require('/public/img/icons/bowl.svg'),
+			vegIcon: require('/public/img/icons/vegetable.svg'),
+			rewardPoints: 10,
+			displayList: [],
+		};
+	},
 };
 </script>
 <style>
 .svg {
-  width: 80px;
-  height: 80px;
+	width: 80px;
+	height: 80px;
 }
 .cardReward {
-  border: 3px solid black !important;
-  padding: 20px;
-  margin-bottom: 20px;
+	border: 3px solid black !important;
+	padding: 20px;
+	margin-bottom: 20px;
 }
 .redeemBtn {
-  width: fit-content;
-  /* align-items: flex-end; */
-  float: right;
+	width: fit-content;
+	/* align-items: flex-end; */
+	float: right;
 }
 </style>
